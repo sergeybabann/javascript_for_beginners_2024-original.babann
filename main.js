d750e0a68d390aa11269a68ccff23806bd15bc52
sergeybabann <sergeybabann@yandex.ru>
2024-02-16 02:30:01 +0300

    additions

---
+++ main.js
@@ -0,0 +1,11 @@
+//---------- Переменные ----------//
+
+
+let userName = 'Марк';
+console.log(userName);
+
+userName = 'Егор';
+console.log(userName);
+
+userName = 'Анжела';
+console.log(userName); 
